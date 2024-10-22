17eab74373ec09f0864a046c8c619f1879da3d7a
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2024-10-22 18:30:34 +0000

    .

--- practicas/practica6/ex4/index.js
+++ practicas/practica6/ex4/index.js
@@ -1,15 +1,14 @@
-document.querySelector("#btnGenerarContrasena").addEventListener("click", function(){
+document.querySelector("#btnGenerarContrasena").addEventListener("click", function() {
     function contrasena() {
-        let letras = 'abcdefghijklmnopqrstuvwxyz'
-        let contrasenaAleatoria
+        let letras = 'abcdefghijklmnopqrstuvwxyz';
+        let contrasenaAleatoria = ''
 
         for (let i = 0; i < 10; i++) {
-            let letra = letras.charAt(Math.floor(Math.random() * letras.length));
-
-            contrasenaAleatoria += letra
-
+            let letra = Math.floor(Math.random() * letras.length)
+            contrasenaAleatoria += letras.charAt(letra)
         }
+
         document.querySelector("#inputContrasena").value = contrasenaAleatoria
     }
-    contrasena()        
-})
+    contrasena()  
+});
