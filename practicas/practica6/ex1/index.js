7554463126e8b2ed3a0236cace9e533a5c4129d4
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2024-10-22 18:09:10 +0000

    .

--- practicas/practica6/ex1/index.js
+++ practicas/practica6/ex1/index.js
@@ -3,12 +3,16 @@ console.log("HOLA")
 document.querySelector(".btnEnviar").addEventListener("click", function(event){
     event.preventDefault()
 
-    numero1 = document.querySelector("#num1").value
+    num1 = document.querySelector("#num1").value
 
-    numero2 = document.querySelector("#num2").value
+    num2 = document.querySelector("#num2").value
+
+    numero1 = parseInt(num1)
+
+    numero2 = parseInt(num2)
 
     function numeroAleatorio(numero1, numero2){
-        numeroAleatorio = Math.random() * (numero2 - numero1 + 1) + numero1;
+        numeroAleatorio = Math.random() * (numero2 - numero1) + numero1;
         console.log(numeroAleatorio)
        return document.querySelector("#numeroAleatorio").innerHTML = numeroAleatorio
     }
